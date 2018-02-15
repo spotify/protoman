bcd435bee29347f9d4f3144b00e7fcaca73d0388
Johan Haals <johan.haals@gmail.com>
2018-02-15 10:06:43 +0100

    Get GCS in a working state

--- registry/src/main/java/com/spotify/protoman/storage/GCS.java
+++ registry/src/main/java/com/spotify/protoman/storage/GCS.java
@@ -1,24 +1,33 @@
 package com.spotify.protoman.storage;
 
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
 public class GCS {
 
-  private final Storage gcs;
+  private final Storage storage;
   private final String bucket;
 
   public GCS(String bucket) {
     this.bucket = bucket;
-    this.gcs = StorageOptions.getDefaultInstance().getService();
+    this.storage = StorageOptions.getDefaultInstance().getService();
   }
 
-  public void get(String path) {
+  public byte[] get(String path) {
+
+    Blob blob = storage.get(BlobId.of(bucket, path));
+    if (blob == null) {
+      throw new RuntimeException(path +" not found");
+    }
+    return blob.getContent();
   }
 
-  public void put(String path, Byte content) {
-    gcs.create(BlobInfo.newBuilder(bucket, path)
-        .build(), "foobar".getBytes());
+  public void put(String path, byte[] content) {
+    storage.create(BlobInfo.newBuilder(bucket, path)
+        .setContentType("text/plain")
+        .build(), content);
   }
 }
