eefa9c83dcbb9f095c6565f6da669d9b8614cce3
fredrikd <fredrikd@spotify.com>
2018-02-23 13:26:04 +0100

    Remove cloud sql storage

--- registry/src/main/java/com/spotify/protoman/registry/Main.java
+++ registry/src/main/java/com/spotify/protoman/registry/Main.java
@@ -5,20 +5,15 @@ import com.spotify.protoman.validation.DefaultSchemaValidator;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import java.io.IOException;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
 
   private static final int GRPC_PORT = 8080;
 
-  private static final String DB_NAME = "postgres";
-  private static final String DB_USER = "postgres";
-  private static final String DB_PASSWORD = "aiF1ahk7meech8ie";
-  private static final String CLOUD_SQL_INSTANCE_CONNECTION_NAME =
-      "fabric-rnd:europe-west1:protoman-test";
+  private static final String BUCKET_NAME = "protoman";
 
-  public static void main(final String...args) throws IOException, SQLException {
+  public static void main(final String... args) throws IOException, SQLException {
     final SchemaRegistry schemaRegistry = createSchemaRegistry();
 
     final SchemaRegistryService service = SchemaRegistryService.create(schemaRegistry);
@@ -35,15 +30,9 @@ public class Main {
   }
 
   private static SchemaRegistry createSchemaRegistry() {
-    final String jdbcUrl = String.format(
-        "jdbc:postgresql://google/%s?socketFactory=com.google.cloud.sql.postgres.SocketFactory"
-        + "&socketFactoryArg=%s",
-        DB_NAME,
-        CLOUD_SQL_INSTANCE_CONNECTION_NAME
-    );
 
-    final SchemaStorage schemaStorage = SqlSchemaStorage.create(
-        () -> DriverManager.getConnection(jdbcUrl, DB_USER, DB_PASSWORD)
+    final SchemaStorage schemaStorage = new SchemaStorageAdapter(
+        new GcsSchemaStorage(BUCKET_NAME)
     );
 
     return SchemaRegistry.create(
