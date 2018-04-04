75fe2dd60da77032e7822146215c8542c3b802ea
fredrikd <fredrikd@spotify.com>
2018-04-04 09:49:06 +0200

    Remove spotify internal jersey plugins

--- registry/src/main/java/com/spotify/protoman/registry/Main.java
+++ registry/src/main/java/com/spotify/protoman/registry/Main.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,6 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableSet;
-import com.spotify.jersey.support.JerseyServerComponentFactory;
 import com.spotify.protoman.descriptor.ProtocDescriptorBuilder;
 import com.spotify.protoman.registry.http.CORSFilter;
 import com.spotify.protoman.registry.http.ProtobufJsonCodec;
@@ -186,7 +185,6 @@ public class Main {
   private static ResourceConfig resourceConfig(SchemaRegistry schemaRegistry) {
     return new ResourceConfig()
         .setApplicationName("protoman")
-        .registerInstances(JerseyServerComponentFactory.defaultComponents())
         .register(ProtobufJsonCodec.class)
         .register(CORSFilter.class)
         .register(SchemaResource.create(schemaRegistry));
